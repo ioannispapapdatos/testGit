b90a623609c1a2b718d3a1ebabb10be1a51e1afe
webmasters <jannispapadatos@gmail.com>
2021-07-30 00:51:48 +0300

    new update

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 
-const randNumber = Math.random() * 20
+// const randNumber = Math.random() * 20
 
-console.log(randNumber);
+// console.log(randNumber);
 
+
+var a = ['dog', 'cat', 'hen']
+a[100] = 'fox'
+
+
+console.log(a.length);
 
 
 
